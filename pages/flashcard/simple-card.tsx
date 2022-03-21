c4f0e4cdb3793565fd93baf22ddab1ea49e8f001
Kevin Gauvain <kevin.gauvain@decathlon.com>
2022-03-21 17:24:42 +0100

    pas de changement ajout de brouillon

--- pages/flashcard/simple-card.tsx
+++ pages/flashcard/simple-card.tsx
@@ -24,3 +24,22 @@ const SimpleCard = () => {
 };
 
 export default SimpleCard;
+
+// ---------------------------Brouillon--------------------------------------------------------
+// -
+// -------Pour random----------
+// -
+// const randomQuestion = Math.floor(Math.random() * panelQuestion.length);
+// const questionRandom = panelQuestion[randomQuestion];
+// const result = questionRandom;
+
+// return (
+//   <div>
+//     <Layout>
+//       <br />
+//       {/* <p>{showingOneFaceOfCard}</p> */}
+//       <button onClick={() => setShowingOneFaceOfCard(!showingOneFaceOfCard)}>
+//         {showingOneFaceOfCard ? (
+//           <p>{result.question}</p>
+//         ) : (
+//           <p>{result.answer}</p>
